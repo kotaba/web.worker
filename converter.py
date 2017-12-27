29426cfaca9aa4971ef8efa4e16b647e7b9e32cc
kotaba <prosperodesu@live.com>
2017-12-27 19:37:01 +0200

    Update

--- converter.py
+++ converter.py
@@ -6,13 +6,19 @@ import json
 import platform
 import os
 import requests
+import re
+import math
 
-
+target_files = {}
+part_files = {}
 HOST = platform.node()
 TASK_GET_URL = 'http://ecsv.org.ua:8001/task/get?hostname=' + HOST
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
-TASK_WEBHOOK_FINISH = 'http://converter.ecsv.org.ua/webhook/video/accept'
+TASK_WEBHOOK_FINISH_VIDEO = 'http://converter.ecsv.org.ua/webhook/video/accept'
+TASK_WEBHOOK_FINISH_PART = 'http://converter.ecsv.org.ua/webhook/part/accept'
 TASK_MANAGER_FINISH = 'http://ecsv.org.ua:8001/webhook/vm/shutdown'
+length_regexp = 'Duration: (\d{2}):(\d{2}):(\d{2})\.\d+,'
+re_length = re.compile(length_regexp)
 
 output = json.load(urllib.urlopen(TASK_GET_URL))
 filename, file_extension = os.path.splitext(output.get('file_url'))
@@ -26,25 +32,73 @@ with open(path, 'wb') as output:
 
 output_file_name = str(task_id) + '.mp4'
 output_base_path = BASE_PATH + '/videos/converted_'
-output_file_path =  output_base_path + output_file_name
+output_file_path = output_base_path + output_file_name
 
-command = "ffmpeg -i %s -c:v libx264 -crf 19 -preset:v superfast -c:a aac -b:a 128k -ac 2 -threads 0 -r 10 -strict -2 %s" % (path, output_file_path)
+command = "ffmpeg -i %s -c:v libx264 -crf 19 -preset:v superfast -c:a aac -b:a 128k -ac 2 -r 10 -strict -2 %s" % (
+path, output_file_path)
 os.system(command)
 
-response = requests.post(TASK_WEBHOOK_FINISH,
-                         files={
-                             'full': open(output_file_path),
-                             'part1': open(output_file_path),
-                             'part2': open(output_file_path)
-                         },
+target_files['full'] = open(output_file_path)
+
+split_length = 10
+output = subprocess.Popen("ffmpeg -i '" + output_file_path + "' 2>&1 | grep 'Duration'",
+                          shell=True,
+                          stdout=subprocess.PIPE
+                          ).stdout.read()
+
+
+matches = re_length.search(output)
+
+try:
+    if matches:
+        video_length = int(matches.group(1)) * 3600 + \
+                    int(matches.group(2)) * 60 + \
+                    int(matches.group(3))
+        print "Video length in seconds: " + str(video_length)
+    else:
+        print "Can't determine video length."
+        raise Exception
+    split_count = int(math.ceil(video_length / float(split_length)))
+    print split_count
+    if split_count > 1:
+        split_cmd = "ffmpeg -i '%s' " % output_file_path
+        try:
+            filebase = ".".join(output_file_path.split(".")[:-1])
+            fileext = output_file_path.split(".")[-1]
+        except IndexError as e:
+            raise IndexError("No . in filename. Error: " + str(e))
+        for n in range(0, split_count):
+            split_str = ""
+            if n == 0:
+                split_start = 0
+            else:
+                split_start = split_length * n
+
+            split_str += " -strict -2 -ss " + str(split_start) + " -t " + str(split_length) + \
+                 " '" + filebase + "-" + str(n) + "." + fileext + \
+                 "'"
+            print "About to run: " + split_cmd + split_str
+            output = subprocess.Popen(split_cmd + split_str, shell=True, stdout=
+            subprocess.PIPE).stdout.read()
+            part_files['part_' + str(n)] = open(filebase + "-" + str(n) + "." + fileext)
+except Exception as e:
+    print e
+
+finishVideoData = requests.post(TASK_WEBHOOK_FINISH_VIDEO,
+                         files=target_files,
                          data={'queue_id': queue_id}
                          )
-json_response = response.text
 
-response = requests.post(TASK_MANAGER_FINISH,
+saved_video_data = finishVideoData.json()
+
+video_id = saved_video_data.get('video_id')
+
+response = requests.post(TASK_WEBHOOK_FINISH_PART,
+                         files=part_files,
+                         data={'video_id': video_id}
+                         )
+print response.text
+
+requests.post(TASK_MANAGER_FINISH,
                          data={'hostname': HOST}
                          )
-
-
-
-
